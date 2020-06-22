73b9f8ec3b5e5b05ab8e740ea97f62a5c96e413a
Jon <durrjp1@gmail.com>
2020-06-22 10:52:52 -0500

    positive vs. negative sentiment analyzer with ML.NET

---
+++ PositiveNegativeSentiment/Program.cs
@@ -0,0 +1,27 @@
+﻿using PositiveNegativeSentimentML.Model;
+using System;
+
+namespace PositiveNegativeSentiment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Add input data
+            var input = new ModelInput();
+            input.Comment = Console.ReadLine();
+
+            // Load model and predict output of sample data
+            ModelOutput result = ConsumeModel.Predict(input);
+            if(result.Prediction == "1")
+            {
+                Console.WriteLine($"Text: {input.Comment}\nThis feedback is negative");
+            }
+            else
+            {
+                Console.WriteLine($"Text: {input.Comment}\nThis feedback is positive!");
+            }
+            
+        }
+    }
+}
